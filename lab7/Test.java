36027da3402b543da4e82fee9a62dababbf38fe8
tran vu <21130616@st.hcmuaf.edu.vn>
2023-11-20 22:28:50 +0700

    thanhvuUpdate

---
+++ lab7/Test.java
@@ -0,0 +1,8 @@
+package lab7;
+public class Test {
+	public static void main(String[] args) {
+		GA_NQueenAlgo test = new GA_NQueenAlgo();
+		Node node = test.execute();
+		node.displayBoard();
+	}
+}
